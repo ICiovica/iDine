cda750be8cf3d085155e3b15f8aac265e0d2f40c
Ionut Ciovica <ionut.ciovica@gmail.com>
2021-12-11 18:03:49 +0000

    First Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Ionut Ciovica on 11/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items) { item in
+                            ItemRow(item: item)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(.grouped)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Ionut Ciovica on 11/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
