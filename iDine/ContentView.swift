63ae5c3c5bc78048c01075eb8428c5bda8468640
Ionut Ciovica <ionut.ciovica@gmail.com>
2021-12-13 12:04:12 +0000

    ItemDetail

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(menu) { section in
                     Section(header: Text(section.name)) {
                         ForEach(section.items) { item in
-                            ItemRow(item: item)
+                            NavigationLink(destination: ItemDetail(item: item)) {
+                                ItemRow(item: item)
+                            }
                         }
                     }
                 }
@@ -28,6 +31,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
     }
 }
